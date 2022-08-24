0f9a07625b4a2b86003ee412b0e6ce896c935e25
Authns <color59abe@gmail.com>
2022-08-25 01:36:28 +0900

    mv files

---
+++ dotfiles/fish/config.fish
@@ -0,0 +1,54 @@
+set -x GOPATH $HOME/dev
+fish_add_path $GOPATH/bin
+
+set -g theme_display_git_master_branch yes
+
+if type -q exa
+    alias ls 'exa --icons --git'
+    alias la "ls -a"
+    alias lla "ll -a"
+end
+
+function fish_user_key_bindings
+  bind \c] peco_select_ghq      # Ctrl-]
+  bind \cr peco_select_history  # Ctrl-r
+end
+
+function peco_select_ghq
+  set -l query (commandline)
+  if test -n $query
+    set peco_flags --query "$query"
+  end
+
+  ghq list -p | peco $peco_flags | read line
+  if test $line
+    cd $line
+    commandline -f repaint
+  end
+end
+
+function peco_select_history
+  set -l query (commandline)
+  if test -n $query
+    set peco_flags --query "$query"
+  end
+
+  history | peco $peco_flags | read line
+  if test $line
+    commandline $line
+  else
+    commandline ''
+  end
+end
+
+alias vim=nvim
+alias ga="git add"
+alias gb="git branch"
+alias gbd="git branch -d"
+alias gcm="git commit -m"
+alias gca="git commit --amend"
+alias gc="git checkout"
+alias gcb="git checkout -b"
+
+fish_user_key_bindings
+
--- scripts/link.sh
+++ scripts/link.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
+set -euo
+
 DIR_ROOT=$(cd $(dirname $0)/.. && pwd)
+FILENAME_LINKLIST="linklist.txt"
 
 # make symbolic link
 
-echo $DIR_ROOT
+DIR_DOTFILES="${DIR_ROOT}/dotfiles"
+FILE_LINKLIST="${DIR_ROOT}/${FILENAME_LINKLIST}"
+cd "${DIR_DOTFILES}" 
+
+if [ ! -r "${FILE_LINKLIST}" ] ; then
+  return
+fi
+
+__remove_linklist_comment() {
+  # '#'以降と空白行を削除
+  sed -e 's/\s*#.*//' \
+    -e '/^\s*$/d' \
+    $1
+}
+
+
+while read -r target link
+do
+  target=$(eval echo "${PWD}/${target}")
+  link=$(eval echo "${link}")
+
+  mkdir -p $(dirname ${link})
+  if [ -d "${link}" ] ; then
+    rm -rf "${link}"
+  fi
+  ln -fsn ${target} ${link}
+done < <( __remove_linklist_comment "${FILE_LINKLIST}" )
+
