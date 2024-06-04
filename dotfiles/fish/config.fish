c3a4fc0d79dba66cf9a0bcb82e9decf6fceac0e5
abe.shunta <abe.shunta@fujitsu.com>
2024-06-04 04:28:18 +0000

    add  Brewfile

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -6,15 +6,56 @@ fish_add_path --path --append /home/linuxbrew/.linuxbrew/sbin
 
 set -g theme_display_git_master_branch yes
 
+abbr --add sc "source ~/.config/fish/config.fish"
+abbr --add np --set-cursor "no_proxy=\"%\""
+
 if type -q eza
-    alias ls 'eza --icons --git'
-    alias la "ls -a"
-    alias lla "ll -a"
+    abbr --add e 'eza --icons --git'
+end
+
+if type -q tmux
+    abbr --add ide 'tmux split-window -v; tmux resize-pane -D 15'
+end
+
+if type -q git
+    abbr --add ga 'git add'
+    abbr --add gb "git branch"
+    abbr --add gd "git diff"
+    abbr --add gf "git fetch"
+    abbr --add gr "git rebase"
+    abbr --add gbd "git branch -d"
+    abbr --add gcm --set-cursor "git commit -m \"%\""
+    abbr --add gca "git commit --amend"
+    abbr --add gst "git status"
+    abbr --add gsw "git switch"
+end
+
+if type -q ansible
+    abbr --add ap 'ansible-playbook'
+end
+
+if type -q brew
+    abbr --add bi 'brew install'
+    abbr --add bd 'brew doctor'
 end
 
 function fish_user_key_bindings
   bind \c] peco_select_ghq      # Ctrl-]
   bind \cr peco_select_history  # Ctrl-r
+end
+
+function sync
+    if type -q apt
+        sudo apt update
+        sudo apt upgrade -y
+        sudo apt autoclean
+        sudo apt autoremove -y
+    end
+
+    if type -q brew
+        brew upgrade
+        brew cleanup --prune 7
+    end
 end
 
 function peco_select_ghq
@@ -44,16 +85,6 @@ function peco_select_history
   end
 end
 
-alias vim=nvim
-alias ide=~/ide
-alias ga="git add"
-alias gb="git branch"
-alias gd="git diff"
-alias gbd="git branch -d"
-alias gcm="git commit -m"
-alias gca="git commit --amend"
-alias gst="git status"
-alias gsw="git switch"
 
 fish_user_key_bindings
 
